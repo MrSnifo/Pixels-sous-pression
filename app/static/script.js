6964c3e9f9ff19f1d99f7d904d03f9c521a2e9fa
Snifo <snifo@mail.com>
2024-12-06 01:01:32 +0100

    core

--- app/static/script.js
+++ app/static/script.js
@@ -22,12 +22,29 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
 
                 const data = await response.blob();
                 const resultDiv = document.getElementById('result');
+
+
+                // Make the download button visible
                 const url = URL.createObjectURL(data);
                 resultDiv.innerHTML = `<h2>Uploaded Image:</h2><img src="${url}" alt="Uploaded Image">`;
+
+                    const downloadButton = document.getElementsByClassName('Download')[0];
+                    downloadButton.style.display = "block";
+
+                    // Set up the download functionality
+                    downloadButton.onclick = function() {
+                        const link = document.createElement('a');
+                        link.href = url;
+                        link.download = `${url}.webp`; // You can customize the filename here
+                        link.click(); // This will trigger the download
+                    };
+
+
 
             } catch (error) {
                 console.error("Error during image upload:", error);
                 const resultDiv = document.getElementById('result');
                 resultDiv.innerHTML = "<p>Error uploading image. Please try again later.</p>";
             }
+
         });
